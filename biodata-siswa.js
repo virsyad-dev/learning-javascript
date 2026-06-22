89746f17c45b06442dbe9ce33ab1417dcd8b8c2d
virsyad-dev <virsyad.dev@gmail.com>
2026-06-22 22:36:19 +0700

    Add duplicate id detection

--- biodata-siswa.js
+++ biodata-siswa.js
@@ -11,13 +11,13 @@ let students = [
 function displayStudents(allStudentData) {
   allStudentData.forEach((studentData) => {
     console.log("=====================");
-    
+
     console.log("id         :" + studentData.id);
     console.log("name       :" + studentData.name);
     console.log("age        :" + studentData.age);
     console.log("major      :" + studentData.major);
 
-    console.log ("Skills     :")
+    console.log("Skills     :");
 
     studentData.skills.forEach((skill, index) => {
       console.log(`${index + 1}. ${skill}`);
@@ -27,33 +27,50 @@ function displayStudents(allStudentData) {
   });
 }
 
-function addNewStudent(newStudent) {
-  students = [...students, ...newStudent];
+function addNewStudents(newStudents) {
+  const clearStudent = []
+
+  newStudents.forEach(newStudent => {
+    const existsInDatabase = students.some(student => student.id === newStudent.id);
+
+    const existsInQueue = clearStudent.some(student => student.id === newStudent.id);
+
+    const isDuplicate = existsInDatabase || existsInQueue;
+    
+    if(!isDuplicate) {
+      clearStudent.push(newStudent);
+      console.log(`Aman: Tidak ada duplikasi untuk ID ${newStudent.id} (${newStudent.name})`);
+    } else {
+          console.log(`Terdapat duplikasi id ${newStudent.id} ${newStudent.name}`)
+    }
+  })
+
+  students = [...students, ...clearStudent];
 
   console.log(
-`Berhasil ditambahkan ${newStudent.length} data siswa baru kedalam database`  );
+    `Berhasil ditambahkan ${newStudents.length} data siswa baru kedalam database`,
+  );
 }
 
 function findStudentById(idSiswa) {
-  const findId = students.find(student => student.id === idSiswa)
+  const findId = students.find((student) => student.id === idSiswa);
 
   if (findId) {
-    console.log("Data berhasil ditemukan")
+    console.log("Data berhasil ditemukan");
+    console.log(findId)
   } else {
-  console.log("Data tidak Ditemukan");
+    console.log("Data tidak Ditemukan");
   }
-
-  return findId;
 }
 
 const newStudents = [
   { id: 2, name: "Budi", age: 18, major: "Finance", skills: ["Math"] },
-  { id: 3, name: "Siti", age: 19, major: "Design", skills: ["UI/UX"] },
-  { id: 4, name: "Andi", age: 18, major: "Informatics", skills: ["Python"] },
+  { id: 2, name: "Siti", age: 19, major: "Design", skills: ["UI/UX"] },
+  { id: 1, name: "Andi", age: 18, major: "Informatics", skills: ["Python"] },
 ];
 
 displayStudents(students);
 
-addNewStudent(newStudents);
+addNewStudents(newStudents);
 
-console.log(findStudentById(1));
+// findStudentById(1)
